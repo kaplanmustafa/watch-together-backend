f5fa174e16a64358de1b192a454fb49bb1729827
Mustafa KAPLAN <mstfakapln@gmail.com>
2024-08-31 15:05:34 +0300

    refactor: add nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 3001;
+  const url = `http://localhost:${port}`;
+
+  await app.listen(port);
+  Logger.log(`Application is running on: ${url}`);
 }
 bootstrap();
